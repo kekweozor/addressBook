94ea71d831da13f775de368b23d079ebdfe6ac19
kekweozor <nwora21@icloud.com>
2024-09-23 12:20:56 -0400

    Added commands to add and remove info from AddressBook.java

--- src/addressBook.java
+++ src/addressBook.java
@@ -2,35 +2,42 @@ import java.util.ArrayList;
 
 public class addressBook {
     public ArrayList<BuddyInfo> addressList;
-    private static BuddyInfo info1 = new BuddyInfo("Ryan", "2232 Frenchie Street", "6136129941");
-    private static BuddyInfo info2 = new BuddyInfo("Thomas", "565 Riley Crescent", "6478882229");
-    private static addressBook addressBook = new addressBook();     //instantiated an object of the class addressbook
+    private static BuddyInfo info1;
+    private static BuddyInfo info2;
 
-
+    public void printName(int i){
+        System.out.println(addressList.get(i).getName());
+    }
     /**
      * Main function stores info1 and info2 buddy info in
      * the collection called arrayList
      *
      * */
     public static void main(String[] args) {
+        info1 = new BuddyInfo("Ryan", "2232 Frenchie Street", "6136129941");
+        info2 = new BuddyInfo("Thomas", "565 Riley Crescent", "6478882229");
+        addressBook abook = new addressBook();     //instantiated an object of the class addressbook
 
-        addressBook.addBuddy(info1);
-        addressBook.addBuddy(info2);
-        addressBook.removeBuddy(info1);
-        System.out.println(addressBook.addressList.get(1).getName());
+        abook.addBuddy(info1);
+        //abook.addBuddy(info2);
+        //abook.removeBuddy(0);
+        abook.printName(0);
+        //abook.printName(1);
         System.out.println("done!");
         System.out.println("Address Book");
     }
     public addressBook() {
-        this.addressList = new ArrayList<>();
+        addressList = new ArrayList<>();
     }
     
     public void addBuddy(BuddyInfo infoLog){
-        this.addressList.add(infoLog);
+        if(infoLog != null) {
+            addressList.add(infoLog);
+        }
     }
 
-    public void removeBuddy(BuddyInfo infoLog){
-        this.addressList.remove(infoLog);
+    public void removeBuddy(int index){
+        addressList.remove(index);
     }
 
 }
